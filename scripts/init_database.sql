9701f8325a2fb76ed04f300b19de789f383fb83b
TakudzwaMotsi <155077413+TakudzwaMotsi@users.noreply.github.com>
2026-05-22 13:27:49 +0200

    Create DataWarehouse database with schemas
    Initialize the DataWarehouse database and create schemas.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,18 @@
-
+use master;
+go
+if exists (select 1 from sys.databases where name = 'DataWarehouse')
+  begin
+    alter database DataWarehouse set SINGLE_USER with rollback immediate;
+    drop database DataWarehouse;
+end;
+go
+create database DataWarehouse;
+go
+use DataWarehouse;
+go
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
